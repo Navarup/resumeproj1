aa18064112c6d97c69d71d19babafe7670ebbb82
Navarup <navyrony57@gmail.com>
2026-01-03 15:40:17 +0530

    Update backend.tf

--- lessons/day26/staging/backend.tf
+++ lessons/day26/staging/backend.tf
@@ -8,5 +8,14 @@ terraform {
 }
 
 provider "azurerm" {
-  features {}
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy = false
+    }
+  }
+
+  # Optional manual SP for local runs
+  subscription_id = length(var.client_id) > 0 ? var.SUB_ID : null
+  client_id       = length(var.client_id) > 0 ? var.client_id : null
+  client_secret   = length(var.client_secret) > 0 ? var.client_secret : null
 }
